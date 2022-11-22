6cf55fbb39883636b95311fc83221a3d207b56f8
Ajay chouhan <102614380+Ajaych19@users.noreply.github.com>
2022-11-22 15:36:09 +0530

    Add files via upload

---
+++ Add.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+class Add
+{
+    static void add(int x,int y)
+    {
+       int sum=x+y;
+       System.out.println(sum);
+    }
+
+     
+    public static void main(String []args)
+    {
+        int x=5;
+        int y=9;
+       add(5,9);
+    } 
+}
